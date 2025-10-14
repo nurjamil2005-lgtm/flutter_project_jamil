74c286af479cb71046d207228c7ebb581ea8ac2a
github <>
2025-10-14 12:58:45 +0800

    Stories-Jamil

--- facebook/lib/pages/newsfeed.dart
+++ facebook/lib/pages/newsfeed.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
+import 'package:facebook/model/story_model.dart';
 import 'package:facebook/widgets/createpost.dart';
+import 'package:facebook/widgets/storieslist.dart';
 import 'package:flutter/material.dart';
 
 class NewsFeed extends StatefulWidget {
@@ -9,6 +13,50 @@ class NewsFeed extends StatefulWidget {
 }
 
 class _NewsFeedState extends State<NewsFeed> {
+  // array
+  List<StoryModel> stories = [
+      StoryModel(
+        id: '1', 
+        username: 'Jemi', 
+        profileImagePath: 'assets/profile/prof1.jpg', 
+        storyImagePath: 'assets/myday/myday1.jpg', 
+        timestamp: DateTime.now().subtract(const Duration(hours: 5)), 
+        isViewed: false),
+
+      StoryModel(
+        id: '2', 
+        username: 'Loki', 
+        profileImagePath: 'assets/profile/prof2.jpg', 
+        storyImagePath: 'assets/myday/myday2.jpg', 
+        timestamp: DateTime.now().subtract(const Duration(hours: 10)), 
+        isViewed: false),
+
+      StoryModel(
+        id: '3', 
+        username: 'Nur', 
+        profileImagePath: 'assets/profile/prof3.jpg', 
+        storyImagePath: 'assets/myday/myday2.jpg', 
+        timestamp: DateTime.now().subtract(const Duration(hours: 15)), 
+        isViewed: false),
+
+      StoryModel(
+        id: '4', 
+        username: 'Lighton', 
+        profileImagePath: 'assets/profile/prof4.jpg', 
+        storyImagePath: 'assets/myday/myday4.jpg', 
+        timestamp: DateTime.now().subtract(const Duration(hours: 20)), 
+        isViewed: false),
+
+      StoryModel(
+        id: '5', 
+        username: 'Emem', 
+        profileImagePath: 'assets/profile/prof5.jpg', 
+        storyImagePath: 'assets/myday/myday5.jpg', 
+        timestamp: DateTime.now().subtract(const Duration(hours: 24)), 
+        isViewed: false),
+  ];
+
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,7 +74,13 @@ class _NewsFeedState extends State<NewsFeed> {
       ),
 
       body: Column(
-        children: [Createpost()],
+        children: [
+          const Createpost(),
+          StoriesList(
+            stories : stories,
+            onStoryTap : (story) {},
+          )
+        ],
       ),
     );
   }
--- facebook/lib/widgets/storycard.dart
+++ facebook/lib/widgets/storycard.dart
@@ -2,7 +2,7 @@ import 'package:facebook/model/story_model.dart';
 import 'package:flutter/material.dart';
 
 class StoryCard extends StatelessWidget {
-  final StoryModel story; 
+  final StoryModel story;
   final VoidCallback? onTap;
   final double width;
   final double height;
@@ -11,8 +11,9 @@ class StoryCard extends StatelessWidget {
     super.key,
     required this.story,
     this.onTap,
-    required this.width,
-    required this.height});
+    this.width = 120,
+    this.height = 200,
+  });
 
   @override
   Widget build(BuildContext context) {
@@ -24,17 +25,77 @@ class StoryCard extends StatelessWidget {
         margin: const EdgeInsets.symmetric(horizontal: 4),
         decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12),
-          border: 
-          story.isViewed ? Border.all(color: Colors.grey.shade300): null,
+          gradient: story.isViewed
+              ? null
+              : const LinearGradient(
+                  colors: [Colors.purple, Colors.pink, Colors.orange],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                ),
+          border: story.isViewed
+              ? Border.all(color: Colors.grey.shade300, width: 2)
+              : null,
         ),
         child: Container(
           margin: const EdgeInsets.all(2),
           decoration: BoxDecoration(
             borderRadius: BorderRadius.circular(10),
             image: DecorationImage(
-              image: AssetImage(story.storyImagePath), fit: BoxFit.cover)),
+              image: AssetImage(story.storyImagePath),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: Container(
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(10),
+              gradient: LinearGradient(
+                begin: Alignment.topCenter,
+                end: Alignment.bottomCenter,
+                colors: [Colors.transparent, Colors.black.withOpacity(0.7)],
+                stops: const [0.6, 1.0],
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    width: 32,
+                    height: 32,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      border: Border.all(color: Colors.white, width: 2),
+                      image: DecorationImage(
+                        image: AssetImage(story.profileImagePath),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                  ),
+                  const Spacer(),
+                  Text(
+                    story.username,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontSize: 12,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    maxLines: 1,
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                  Text(
+                    story.timeAge,
+                    style: TextStyle(
+                      color: Colors.white.withOpacity(0.8),
+                      fontSize: 10,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
   }
-} 
+}
